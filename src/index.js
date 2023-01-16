6c2f3b5a894c955af7b979ca526849031651b188
justincbal <justinseebee@hotmail.com>
2023-01-15 22:56:33 -0500

    add line numbers, and complete skills section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import './styles/main.scss';
 
-const AboutMe = {
-    name: 'Justin Balagtas',
-    location: 24,
-    biography: 
-    'This is a test \
-    I am'
+const navItem = document.querySelectorAll('.nav-item');
 
-    
-}
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        console.log(item);
+
+        for(let i = 0; i < 4; i++) {
+            if(navItem[i].classList.contains('nav-active')) 
+                navItem[i].classList.remove('nav-active');
+        }
+
+        item.classList.add('nav-active');
+
+    })
+})
 
